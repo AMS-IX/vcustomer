1625fbbb89181e779ad33f340c5b1c1ccc892ad1
Joris Claassen <joris@claassen.co>
2016-07-19 16:43:04 +0200

    updated and alphabetized functions

--- docker_rest.py
+++ docker_rest.py
@@ -2,64 +2,6 @@
 import requests
 import json
 
-# function to retreive network information from the docker API
-def InspectNet(rest_api, net_name=""):
-	if net_name == "":
-		r = requests.get(rest_api + "/networks/")
-	else:
-		r = requests.get(rest_api + "/networks/" + net_name)
-	try:
-		assert r.status_code == 200
-		return r.json()
-	except AssertionError:
-		return "HTTP status code not correct!"
-		return ""
-		return "API output:"
-		return r.text
-
-# function to retrieve container information from the docker API
-def InspectCont(rest_api, cont_name=""):
-	if cont_name == "":
-		r = requests.get(rest_api + "/containers/json?all=1")
-	else:
-		r = requests.get(rest_api + "/containers/" + cont_name + "/json")
-	try:
-		assert r.status_code == 200
-		return r.json()
-	except AssertionError:
-		return "HTTP status code not correct!"
-		return ""
-		return "API output:"
-		return r.text
-
-# function to create a new network using the docker API
-def CreateNet(rest_api, net_name, parent_if):
-	payload = {
-			"Name": net_name,
-			"CheckDuplicate": True,
-			"Driver": "macvlan",
-			"EnableIPv6": False,
-			"IPAM": {
-				"Config": [{
-					"Subnet": "10.25.208.0/21"}]
-				},
-			"Options": {
-				"parent": parent_if,
-				"macvlan_mode": "vepa"
-			}
-		} 
-	
-	headers = {"content-type": "application/json"}
-	r = requests.post(rest_api + "/networks/create", data=json.dumps(payload), headers=headers)	
-	try:
-		assert r.status_code == 201
-		return r.json()
-	except AssertionError:
-		return "HTTP status code not correct!"
-		return ""
-		return "API output:"
-		return r.text
-
 # function to create a new container using the docker API
 def CreateCont(rest_api, name, net_name, ip="", mac=""):
 	payload = {
@@ -101,7 +43,7 @@ def CreateCont(rest_api, name, net_name, ip="", mac=""):
 		return "API output:"
 		return r.text
 
-# function to create an excute command for an container
+# function to create an excute command for an container using the docker API
 def CreateExec(rest_api, name, cmd):
 	payload = {
                         "AttachStdin": False,
@@ -127,12 +69,58 @@ def CreateExec(rest_api, name, cmd):
 		return "API output:"
 		return r.text
 
-# function to delete a network using the docker API
-def RemoveNet(rest_api, name):
-	r = requests.delete(rest_api + "/networks/" + name)
+# function to create a new network using the docker API
+def CreateNet(rest_api, net_name, parent_if):
+	payload = {
+			"Name": net_name,
+			"CheckDuplicate": True,
+			"Driver": "macvlan",
+			"EnableIPv6": False,
+			"IPAM": {
+				"Config": [{
+					"Subnet": "10.25.208.0/21"}]
+				},
+			"Options": {
+				"parent": parent_if,
+				"macvlan_mode": "vepa"
+			}
+		} 
+	
+	headers = {"content-type": "application/json"}
+	r = requests.post(rest_api + "/networks/create", data=json.dumps(payload), headers=headers)	
 	try:
-		assert r.status_code == 204
-		return "Network removed succesfully!"
+		assert r.status_code == 201
+		return r.json()
+	except AssertionError:
+		return "HTTP status code not correct!"
+		return ""
+		return "API output:"
+		return r.text
+
+# function to retrieve container information from the docker API
+def InspectCont(rest_api, cont_name=""):
+	if cont_name == "":
+		r = requests.get(rest_api + "/containers/json?all=1")
+	else:
+		r = requests.get(rest_api + "/containers/" + cont_name + "/json")
+	try:
+		assert r.status_code == 200
+		return r.json()
+	except AssertionError:
+		return "HTTP status code not correct!"
+		return ""
+		return "API output:"
+		return r.text
+
+# function to retreive network information from the docker API
+def InspectNet(rest_api, net_name=""):
+	if net_name == "":
+		r = requests.get(rest_api + "/networks/")
+	else:
+		r = requests.get(rest_api + "/networks/" + net_name)
+	try:
+		assert r.status_code == 200
+		return r.json()
 	except AssertionError:
 		return "HTTP status code not correct!"
 		return ""
@@ -145,6 +133,30 @@ def RemoveCont(rest_api, name):
 	try:
 		assert r.status_code == 204
 		return "Container removed succesfully!"
+	except AssertionError:
+		return "HTTP status code not correct!"
+		return ""
+		return "API output:"
+		return r.text
+
+# function to delete a network using the docker API
+def RemoveNet(rest_api, name):
+	r = requests.delete(rest_api + "/networks/" + name)
+	try:
+		assert r.status_code == 204
+		return "Network removed succesfully!"
+	except AssertionError:
+		return "HTTP status code not correct!"
+		return ""
+		return "API output:"
+		return r.text
+
+# function to start a container using the docker API
+def StartCont(rest_api, name):
+	r = requests.post(rest_api + "/containers/" + name + "/start")
+	try:
+		assert r.status_code == 204
+		return "Container started succesfully!"
 	except AssertionError:
 		return "HTTP status code not correct!"
 		return ""
@@ -168,19 +180,7 @@ def StartExec(rest_api, id):
 		return "API output:"
 		return r.text
 
-# function to start a container using the docker API
-def StartCont(name):
-	r = requests.post(rest_api + "/containers/" + name + "/start")
-	try:
-		assert r.status_code == 204
-		return "Container started succesfully!"
-	except AssertionError:
-		return "HTTP status code not correct!"
-		return ""
-		return "API output:"
-		return r.text
-
-def StopCont(name):
+def StopCont(rest_api, name):
 	r = requests.post(rest_api + "/containers/" + name + "/stop")
 	try:
 		assert r.status_code == 204
