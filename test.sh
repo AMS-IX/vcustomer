4ae231f1459bcc169f8096411088a6a77620eabe
jorisc90 <joris@claassen.co>
2016-09-23 16:47:46 +0200

    Delete test.sh

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-python3 request.py create network macvlan1 eth1
-python3 request.py inspect network macvlan1
-python3 request.py create container macvlan1 macvlan1 10.25.208.10 12:34:56:78:9a:bc
-python3 request.py inspect container macvlan1
-python3 request.py start container macvlan1
-python3 request.py delete container macvlan1 
-python3 request.py delete network macvlan1
